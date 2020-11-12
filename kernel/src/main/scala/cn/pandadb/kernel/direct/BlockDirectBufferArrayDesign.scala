17e9164b87d5f97a89c84a0cd132f579434c53cc
LianxinGao <503773453@qq.com>
2020-11-12 14:12:58 +0800

    rename

--- kernel/src/main/scala/cn/pandadb/kernel/direct/BlockDirectBufferArrayDesign.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/direct/BlockDirectBufferArrayDesign.scala
@@ -3,23 +3,23 @@ package cn.pandadb.kernel.direct
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-class GlobalManager() {
-  val deleteLog = mutable.Queue[Int]()
+object DirectMemoryManager{
+  val deleteLog: mutable.Queue[Int] = mutable.Queue[Int]()
   val directBuffer = new ArrayBuffer[EndNodesBlock]()
   val BLOCK_SIZE = 3
 
   def generateBlock(): EndNodesBlock = {
-    new EndNodesBlock(-1, -1, -1, -1, -1, BLOCK_SIZE, deleteLog, GlobalManager.this)
+    new EndNodesBlock(-1, -1, -1, -1, -1, BLOCK_SIZE, deleteLog)
   }
 
-  def generateBlockIndex(): (Int, Boolean) = {
+  def generateBlockId(): (Int, Boolean) = {
     var isFromDelete = false
-    var blockIndex = directBuffer.size
+    var blockId = directBuffer.size
     if (deleteLog.nonEmpty) {
-      blockIndex = deleteLog.dequeue()
+      blockId = deleteLog.dequeue()
       isFromDelete = true
     }
-    (blockIndex, isFromDelete)
+    (blockId, isFromDelete)
   }
 
   def putBlockToDirectBuffer(newBlock: EndNodesBlock, index: Int, isReplace: Boolean): Unit = {
@@ -29,41 +29,42 @@ class GlobalManager() {
       directBuffer += newBlock
     }
   }
-  def getBlock(blockIndex: Int): EndNodesBlock ={
-    directBuffer(blockIndex)
+  def getBlock(blockId: Int): EndNodesBlock ={
+    directBuffer(blockId)
   }
 }
 
-class FromNodeBlockManager(globalManager: GlobalManager) {
-  private var beginBlockIndex = -1
+class OutGoingEdgeBlockManager(startBlockId: Int = -1) {
 
-  def getBeginBlockIndex(): Int = {
-    beginBlockIndex
+  private var beginBlockId = startBlockId
+
+  def getBeginBlockId: Int = {
+    beginBlockId
   }
 
-  def put(nodeId: Long): Unit = {
+  def put(nodeId: Long): Int = {
     // no block
-    if (beginBlockIndex == -1) {
-      val genBlockIndex = globalManager.generateBlockIndex()
+    if (beginBlockId == -1) {
+      val getBlockId = DirectMemoryManager.generateBlockId()
 
-      beginBlockIndex = genBlockIndex._1
-      val newBlock = globalManager.generateBlock()
+      beginBlockId = getBlockId._1
+      val newBlock = DirectMemoryManager.generateBlock()
       newBlock.put(nodeId)
-      newBlock.thisBlockIndex = beginBlockIndex
+      newBlock.thisBlockId = beginBlockId
       newBlock.thisBlockMinNodeId = nodeId
       newBlock.thisBlockMaxNodeId = nodeId
 
-      globalManager.putBlockToDirectBuffer(newBlock, beginBlockIndex, genBlockIndex._2)
+      DirectMemoryManager.putBlockToDirectBuffer(newBlock, beginBlockId, getBlockId._2)
     }
     // have block
     else {
       var flag = true
-      var queryBlock = globalManager.getBlock(beginBlockIndex)
+      var queryBlock = DirectMemoryManager.getBlock(beginBlockId)
       while (flag) {
         val isFound = queryBlockToInsertStrategy(queryBlock, nodeId)
         if (!isFound) {
-          if (queryBlock.thisBlockNextBlockIndex != -1) {
-            queryBlock = globalManager.getBlock(queryBlock.thisBlockNextBlockIndex)
+          if (queryBlock.thisBlockNextBlockId != -1) {
+            queryBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockNextBlockId)
           } else {
             flag = false
             throw new Exception("unknown situation error....please report")
@@ -72,20 +73,21 @@ class FromNodeBlockManager(globalManager: GlobalManager) {
         else flag = false
       }
     }
+    beginBlockId
   }
 
   def queryBlockToInsertStrategy(queryBlock: EndNodesBlock, nodeId: Long): Boolean = {
     var isFound = false
     var preBlock: EndNodesBlock = null
     var nextBlock: EndNodesBlock = null
-    if (queryBlock.thisBlockPreBlockIndex != -1) preBlock = globalManager.getBlock(queryBlock.thisBlockPreBlockIndex)
-    if (queryBlock.thisBlockNextBlockIndex != -1) nextBlock = globalManager.getBlock(queryBlock.thisBlockNextBlockIndex)
+    if (queryBlock.thisBlockPreBlockId != -1) preBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockPreBlockId)
+    if (queryBlock.thisBlockNextBlockId != -1) nextBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockNextBlockId)
 
     if (nodeId > queryBlock.thisBlockMinNodeId && nodeId < queryBlock.thisBlockMaxNodeId) {
       queryBlock.put(nodeId)
       isFound = true
     }
-    else if (queryBlock.arrayUsedSize < globalManager.BLOCK_SIZE) {
+    else if (queryBlock.arrayUsedSize < DirectMemoryManager.BLOCK_SIZE) {
       // put to this block depends on preBlock max and nextBlock min
       if (preBlock == null && nextBlock == null) {
         queryBlock.put(nodeId)
@@ -110,79 +112,79 @@ class FromNodeBlockManager(globalManager: GlobalManager) {
         }
       }
     }
-    else if (queryBlock.arrayUsedSize == globalManager.BLOCK_SIZE) {
+    else if (queryBlock.arrayUsedSize == DirectMemoryManager.BLOCK_SIZE) {
       // new head
       if (preBlock == null && nodeId < queryBlock.thisBlockMinNodeId) {
-        val index = globalManager.generateBlockIndex()
-        val newBlock = globalManager.generateBlock()
+        val index = DirectMemoryManager.generateBlockId()
+        val newBlock = DirectMemoryManager.generateBlock()
         newBlock.put(nodeId)
 
-        newBlock.thisBlockNextBlockIndex = queryBlock.thisBlockIndex
-        newBlock.thisBlockIndex = index._1
+        newBlock.thisBlockNextBlockId = queryBlock.thisBlockId
+        newBlock.thisBlockId = index._1
         newBlock.thisBlockMinNodeId = nodeId
         newBlock.thisBlockMaxNodeId = nodeId
 
-        globalManager.putBlockToDirectBuffer(newBlock, index._1, index._2)
+        DirectMemoryManager.putBlockToDirectBuffer(newBlock, index._1, index._2)
 
-        queryBlock.thisBlockPreBlockIndex = newBlock.thisBlockIndex
-        beginBlockIndex = newBlock.thisBlockIndex
+        queryBlock.thisBlockPreBlockId = newBlock.thisBlockId
+        beginBlockId = newBlock.thisBlockId
 
         isFound = true
       }
       // insert to left
       else if (preBlock != null && nodeId > preBlock.thisBlockMaxNodeId && nodeId < queryBlock.thisBlockMinNodeId) {
-        val index = globalManager.generateBlockIndex()
-        //        val leftBlock = new EndNodesBlock(preBlock.thisBlockIndex, queryBlock.thisBlockIndex, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
-        val leftBlock = globalManager.generateBlock()
+        val index = DirectMemoryManager.generateBlockId()
+        //        val leftBlock = new EndNodesBlock(preBlock.thisBlockId, queryBlock.thisBlockId, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
+        val leftBlock = DirectMemoryManager.generateBlock()
         leftBlock.put(nodeId)
 
-        leftBlock.thisBlockPreBlockIndex = preBlock.thisBlockIndex
-        leftBlock.thisBlockNextBlockIndex = queryBlock.thisBlockIndex
-        leftBlock.thisBlockIndex = index._1
+        leftBlock.thisBlockPreBlockId = preBlock.thisBlockId
+        leftBlock.thisBlockNextBlockId = queryBlock.thisBlockId
+        leftBlock.thisBlockId = index._1
         leftBlock.thisBlockMinNodeId = nodeId
         leftBlock.thisBlockMaxNodeId = nodeId
 
-        globalManager.putBlockToDirectBuffer(leftBlock, index._1, index._2)
+        DirectMemoryManager.putBlockToDirectBuffer(leftBlock, index._1, index._2)
 
-        preBlock.thisBlockNextBlockIndex = leftBlock.thisBlockIndex
-        queryBlock.thisBlockPreBlockIndex = leftBlock.thisBlockIndex
+        preBlock.thisBlockNextBlockId = leftBlock.thisBlockId
+        queryBlock.thisBlockPreBlockId = leftBlock.thisBlockId
 
         isFound = true
       }
       // insert to right
       else if (nextBlock != null && nodeId < nextBlock.thisBlockMinNodeId && nodeId > queryBlock.thisBlockMaxNodeId) {
-        val index = globalManager.generateBlockIndex()
-        //        val rightBlock = new EndNodesBlock(queryBlock.thisBlockIndex, nextBlock.thisBlockIndex, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
-        val rightBlock = globalManager.generateBlock()
+        val index = DirectMemoryManager.generateBlockId()
+        //        val rightBlock = new EndNodesBlock(queryBlock.thisBlockId, nextBlock.thisBlockId, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
+        val rightBlock = DirectMemoryManager.generateBlock()
         rightBlock.put(nodeId)
 
-        rightBlock.thisBlockPreBlockIndex = queryBlock.thisBlockIndex
-        rightBlock.thisBlockNextBlockIndex = nextBlock.thisBlockIndex
-        rightBlock.thisBlockIndex = index._1
+        rightBlock.thisBlockPreBlockId = queryBlock.thisBlockId
+        rightBlock.thisBlockNextBlockId = nextBlock.thisBlockId
+        rightBlock.thisBlockId = index._1
         rightBlock.thisBlockMinNodeId = nodeId
         rightBlock.thisBlockMaxNodeId = nodeId
 
-        globalManager.putBlockToDirectBuffer(rightBlock, index._1, index._2)
+        DirectMemoryManager.putBlockToDirectBuffer(rightBlock, index._1, index._2)
 
-        queryBlock.thisBlockNextBlockIndex = rightBlock.thisBlockIndex
-        nextBlock.thisBlockPreBlockIndex = rightBlock.thisBlockIndex
+        queryBlock.thisBlockNextBlockId = rightBlock.thisBlockId
+        nextBlock.thisBlockPreBlockId = rightBlock.thisBlockId
         isFound = true
       }
       // new tail
       else if (nextBlock == null && nodeId > queryBlock.thisBlockMaxNodeId) {
-        val index = globalManager.generateBlockIndex()
-        //        val tailBlock = new EndNodesBlock(queryBlock.thisBlockIndex, -1, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
-        val tailBlock = globalManager.generateBlock()
+        val index = DirectMemoryManager.generateBlockId()
+        //        val tailBlock = new EndNodesBlock(queryBlock.thisBlockId, -1, index._1, nodeId, nodeId, blockSize, directBuffer, deleteLog)
+        val tailBlock = DirectMemoryManager.generateBlock()
         tailBlock.put(nodeId)
 
-        tailBlock.thisBlockPreBlockIndex = queryBlock.thisBlockIndex
-        tailBlock.thisBlockIndex = index._1
+        tailBlock.thisBlockPreBlockId = queryBlock.thisBlockId
+        tailBlock.thisBlockId = index._1
         tailBlock.thisBlockMinNodeId = nodeId
         tailBlock.thisBlockMaxNodeId = nodeId
 
-        globalManager.putBlockToDirectBuffer(tailBlock, index._1, index._2)
+        DirectMemoryManager.putBlockToDirectBuffer(tailBlock, index._1, index._2)
 
-        queryBlock.thisBlockNextBlockIndex = tailBlock.thisBlockIndex
+        queryBlock.thisBlockNextBlockId = tailBlock.thisBlockId
         isFound = true
       }
     }
@@ -190,47 +192,47 @@ class FromNodeBlockManager(globalManager: GlobalManager) {
   }
 
   def delete(nodeId: Long): Unit = {
-    var queryBlock = globalManager.getBlock(beginBlockIndex)
+    var queryBlock = DirectMemoryManager.getBlock(beginBlockId)
     var flag = true
     while (flag) {
       if (nodeId >= queryBlock.thisBlockMinNodeId && nodeId <= queryBlock.thisBlockMaxNodeId) {
         val remainSize = queryBlock.delete(nodeId)
         if (remainSize == 0) {
           // delete only 1 block
-          if (queryBlock.thisBlockPreBlockIndex == -1 && queryBlock.thisBlockNextBlockIndex == -1) {
-            beginBlockIndex = -1
-            globalManager.deleteLog.enqueue(queryBlock.thisBlockIndex)
+          if (queryBlock.thisBlockPreBlockId == -1 && queryBlock.thisBlockNextBlockId == -1) {
+            beginBlockId = -1
+            DirectMemoryManager.deleteLog.enqueue(queryBlock.thisBlockId)
           }
           // delete head block
-          else if (queryBlock.thisBlockPreBlockIndex == -1) {
-            val nextBlock = globalManager.getBlock(queryBlock.thisBlockNextBlockIndex)
-            beginBlockIndex = nextBlock.thisBlockIndex
-            nextBlock.thisBlockPreBlockIndex = -1
+          else if (queryBlock.thisBlockPreBlockId == -1) {
+            val nextBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockNextBlockId)
+            beginBlockId = nextBlock.thisBlockId
+            nextBlock.thisBlockPreBlockId = -1
 
-            globalManager.deleteLog.enqueue(queryBlock.thisBlockIndex)
+            DirectMemoryManager.deleteLog.enqueue(queryBlock.thisBlockId)
           }
           // delete middle block
-          else if (queryBlock.thisBlockPreBlockIndex != -1 && queryBlock.thisBlockNextBlockIndex != -1) {
-            val preBlock = globalManager.getBlock(queryBlock.thisBlockPreBlockIndex)
-            val nextBlock = globalManager.getBlock(queryBlock.thisBlockNextBlockIndex)
-            preBlock.thisBlockNextBlockIndex = nextBlock.thisBlockIndex
-            nextBlock.thisBlockPreBlockIndex = preBlock.thisBlockIndex
+          else if (queryBlock.thisBlockPreBlockId != -1 && queryBlock.thisBlockNextBlockId != -1) {
+            val preBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockPreBlockId)
+            val nextBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockNextBlockId)
+            preBlock.thisBlockNextBlockId = nextBlock.thisBlockId
+            nextBlock.thisBlockPreBlockId = preBlock.thisBlockId
 
-            globalManager.deleteLog.enqueue(queryBlock.thisBlockIndex)
+            DirectMemoryManager.deleteLog.enqueue(queryBlock.thisBlockId)
           }
           // delete last block
           else {
-            val preBlock = globalManager.getBlock(queryBlock.thisBlockPreBlockIndex)
-            preBlock.thisBlockNextBlockIndex = -1
+            val preBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockPreBlockId)
+            preBlock.thisBlockNextBlockId = -1
 
-            globalManager.deleteLog.enqueue(queryBlock.thisBlockIndex)
+            DirectMemoryManager.deleteLog.enqueue(queryBlock.thisBlockId)
           }
         }
         flag = false
       }
       else {
-        if (queryBlock.thisBlockNextBlockIndex != -1) {
-          queryBlock = globalManager.getBlock(queryBlock.thisBlockNextBlockIndex)
+        if (queryBlock.thisBlockNextBlockId != -1) {
+          queryBlock = DirectMemoryManager.getBlock(queryBlock.thisBlockNextBlockId)
         } else {
           flag = false
         }
@@ -238,15 +240,15 @@ class FromNodeBlockManager(globalManager: GlobalManager) {
     }
   }
 
-  def queryByLink(): Unit = {
-    val startIndex = beginBlockIndex
+  def queryByLink(startBlockId: Int): Unit = {
+    val startIndex = startBlockId
     var flag = true
     if (startIndex != -1) {
-      var block = globalManager.getBlock(startIndex)
+      var block = DirectMemoryManager.getBlock(startIndex)
       while (flag) {
         block.nodeIdArray.foreach(println)
-        if (block.thisBlockNextBlockIndex != -1) {
-          block = globalManager.getBlock(block.thisBlockNextBlockIndex)
+        if (block.thisBlockNextBlockId != -1) {
+          block = DirectMemoryManager.getBlock(block.thisBlockNextBlockId)
         }
         else flag = false
         println("===========================================")
@@ -256,19 +258,19 @@ class FromNodeBlockManager(globalManager: GlobalManager) {
 }
 
 // blockSize should be odd number
-class EndNodesBlock(preBlock: Int, nextBlock: Int, blockIndex: Int, maxNodeId: Long, minNodeId: Long, blockSize: Int,
-                    deleteLog: mutable.Queue[Int], globalManager: GlobalManager) {
+class EndNodesBlock(preBlock: Int, nextBlock: Int, blockId: Int, maxNodeId: Long, minNodeId: Long, blockSize: Int,
+                    deleteLog: mutable.Queue[Int]) {
   val nodeIdArray: Array[Long] = new Array[Long](blockSize)
   var arrayUsedSize = 0
 
-  var thisBlockMinNodeId = minNodeId
-  var thisBlockMaxNodeId = maxNodeId
-  var thisBlockIndex = blockIndex
-  var thisBlockNextBlockIndex = nextBlock
-  var thisBlockPreBlockIndex = preBlock
+  var thisBlockMinNodeId: Long = minNodeId
+  var thisBlockMaxNodeId: Long = maxNodeId
+  var thisBlockId: Int = blockId
+  var thisBlockNextBlockId: Int = nextBlock
+  var thisBlockPreBlockId: Int = preBlock
 
   def put(nodeId: Long): Unit = {
-    if (arrayUsedSize < nodeIdArray.size) {
+    if (arrayUsedSize < nodeIdArray.length) {
       insertToArray(nodeId)
       if (arrayUsedSize != 1) {
         if (nodeId < thisBlockMinNodeId) thisBlockMinNodeId = nodeId
@@ -278,8 +280,8 @@ class EndNodesBlock(preBlock: Int, nextBlock: Int, blockIndex: Int, maxNodeId: L
     // split block
     else {
       val splitAt = blockSize / 2
-      val smallerBlock = globalManager.generateBlock()
-      val biggerBlock = globalManager.generateBlock()
+      val smallerBlock = DirectMemoryManager.generateBlock()
+      val biggerBlock = DirectMemoryManager.generateBlock()
 
       if (nodeId < nodeIdArray(splitAt)) {
         // nodeId belong to smaller block
@@ -298,42 +300,42 @@ class EndNodesBlock(preBlock: Int, nextBlock: Int, blockIndex: Int, maxNodeId: L
       biggerBlock.thisBlockMinNodeId = biggerBlock.nodeIdArray(0)
       biggerBlock.thisBlockMaxNodeId = biggerBlock.nodeIdArray(splitAt)
 
-      val biggerBlockIndex = globalManager.generateBlockIndex()
+      val biggerBlockId = DirectMemoryManager.generateBlockId()
 
       // first block split
-      if (thisBlockPreBlockIndex == -1) {
+      if (thisBlockPreBlockId == -1) {
 
-        smallerBlock.thisBlockNextBlockIndex = biggerBlockIndex._1
-        smallerBlock.thisBlockIndex = thisBlockIndex
+        smallerBlock.thisBlockNextBlockId = biggerBlockId._1
+        smallerBlock.thisBlockId = thisBlockId
 
-        biggerBlock.thisBlockPreBlockIndex = thisBlockIndex
-        biggerBlock.thisBlockIndex = biggerBlockIndex._1
-        if (thisBlockNextBlockIndex != -1) {
-          biggerBlock.thisBlockNextBlockIndex = thisBlockNextBlockIndex
+        biggerBlock.thisBlockPreBlockId = thisBlockId
+        biggerBlock.thisBlockId = biggerBlockId._1
+        if (thisBlockNextBlockId != -1) {
+          biggerBlock.thisBlockNextBlockId = thisBlockNextBlockId
         }
       }
       // middle block split
-      else if (thisBlockPreBlockIndex != -1 && thisBlockNextBlockIndex != -1) {
-        smallerBlock.thisBlockPreBlockIndex = thisBlockPreBlockIndex
-        smallerBlock.thisBlockIndex = thisBlockIndex
-        smallerBlock.thisBlockNextBlockIndex = biggerBlockIndex._1
+      else if (thisBlockPreBlockId != -1 && thisBlockNextBlockId != -1) {
+        smallerBlock.thisBlockPreBlockId = thisBlockPreBlockId
+        smallerBlock.thisBlockId = thisBlockId
+        smallerBlock.thisBlockNextBlockId = biggerBlockId._1
 
-        biggerBlock.thisBlockPreBlockIndex = thisBlockIndex
-        biggerBlock.thisBlockIndex = biggerBlockIndex._1
-        biggerBlock.thisBlockNextBlockIndex = thisBlockNextBlockIndex
+        biggerBlock.thisBlockPreBlockId = thisBlockId
+        biggerBlock.thisBlockId = biggerBlockId._1
+        biggerBlock.thisBlockNextBlockId = thisBlockNextBlockId
       }
       // last block split
       else {
-        smallerBlock.thisBlockPreBlockIndex = thisBlockPreBlockIndex
-        smallerBlock.thisBlockIndex = thisBlockIndex
-        smallerBlock.thisBlockNextBlockIndex = biggerBlockIndex._1
+        smallerBlock.thisBlockPreBlockId = thisBlockPreBlockId
+        smallerBlock.thisBlockId = thisBlockId
+        smallerBlock.thisBlockNextBlockId = biggerBlockId._1
 
-        biggerBlock.thisBlockPreBlockIndex = thisBlockIndex
-        biggerBlock.thisBlockIndex = biggerBlockIndex._1
+        biggerBlock.thisBlockPreBlockId = thisBlockId
+        biggerBlock.thisBlockId = biggerBlockId._1
       }
 
-      globalManager.putBlockToDirectBuffer(smallerBlock, smallerBlock.thisBlockIndex, true)
-      globalManager.putBlockToDirectBuffer(biggerBlock, biggerBlockIndex._1, biggerBlockIndex._2)
+      DirectMemoryManager.putBlockToDirectBuffer(smallerBlock, smallerBlock.thisBlockId, isReplace = true)
+      DirectMemoryManager.putBlockToDirectBuffer(biggerBlock, biggerBlockId._1, biggerBlockId._2)
     }
   }
 
