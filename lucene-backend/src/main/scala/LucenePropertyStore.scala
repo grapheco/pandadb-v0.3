976856881d1d22db39778e2946370346c1b68554
excelwang <wanghuajin999@163.com>
2020-11-23 18:21:23 +0800

    fulfill LucenePropertyStore

--- lucene-backend/src/main/scala/LucenePropertyStore.scala
+++ lucene-backend/src/main/scala/LucenePropertyStore.scala
@@ -5,8 +5,10 @@ import java.io.File
 import cn.pandadb.kernel.{NodeId, PropertyStore, RelationId, TypedId}
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document.{Document, Field, StoredField, StringField, TextField}
-import org.apache.lucene.index.{IndexWriter, IndexWriterConfig, IndexableField, Term}
+import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, IndexableField, Term}
+import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.store.{Directory, FSDirectory}
+import org.apache.lucene.queryparser.classic.QueryParser
 
 class LucenePropertyStore(dir: File) extends PropertyStore {
 
@@ -14,6 +16,9 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
   val analyzer = new StandardAnalyzer
   val conf = new IndexWriterConfig(analyzer)
   val indexWriter = new IndexWriter(directory, conf)
+
+  val reader = DirectoryReader.open(indexWriter)
+  var searcher = new IndexSearcher(reader)
 
   private def documentId(tid: TypedId): String = {
     tid match {
@@ -28,7 +33,6 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
     props.foreach { x =>
       document.add(new StoredField(x._1, x._2.toString))
     }
-
     indexWriter.addDocument(document)
     indexWriter.commit()
   }
@@ -36,6 +40,22 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
   override def delete(id: TypedId): Unit =
     indexWriter.deleteDocuments(new Term("_id", documentId(id)))
 
-  override def lookup(id: TypedId): Option[Map[String, Any]] = ???
-  override def close(): Unit = ???
+  override def lookup(id: TypedId): Option[Map[String, Any]] = {
+    val doc = new collection.mutable.HashMap[String, Any]()
+    val idQuery = new QueryParser("_id", analyzer).parse(documentId(id))
+    val hits = searcher.search(idQuery,1)
+    if (hits.totalHits == 0) return None
+    val itl = searcher.doc(hits.scoreDocs.head.doc).getFields().iterator()
+    while (itl.hasNext()) {
+      val field = itl.next()
+      doc.put(field.name(), field.stringValue())
+    }
+    Some(doc.toMap)
+  }
+
+  override def close(): Unit = {
+    indexWriter.close()
+    reader.close()
+  }
+
 }
