ec83fbe9b1947be21cea7bec68391c3d639e045d
excelwang <wanghuajin999@163.com>
2020-11-24 18:26:51 +0800

    fix lookup by id

--- lucene-backend/src/main/scala/LucenePropertyStore.scala
+++ lucene-backend/src/main/scala/LucenePropertyStore.scala
@@ -4,11 +4,13 @@ import java.io.File
 
 import cn.pandadb.kernel.{NodeId, PropertyStore, RelationId, TypedId}
 import org.apache.lucene.analysis.standard.StandardAnalyzer
-import org.apache.lucene.document.{Document, Field, StoredField, StringField, TextField}
+import org.apache.lucene.document.Field.Store
+import org.apache.lucene.document.{Document, Field, FieldType, StoredField, StringField, TextField}
 import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, IndexableField, Term}
 import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.store.{Directory, FSDirectory}
 import org.apache.lucene.queryparser.classic.QueryParser
+import org.apache.lucene.document.TextField
 
 class LucenePropertyStore(dir: File) extends PropertyStore {
 
@@ -16,9 +18,13 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
   val analyzer = new StandardAnalyzer
   val conf = new IndexWriterConfig(analyzer)
   val indexWriter = new IndexWriter(directory, conf)
-
-  val reader = DirectoryReader.open(indexWriter)
+  var reader = DirectoryReader.open(indexWriter)
   var searcher = new IndexSearcher(reader)
+
+  val termVectoredTextFieldType = new FieldType(TextField.TYPE_STORED)
+  termVectoredTextFieldType.setStoreTermVectors(true)
+  termVectoredTextFieldType.setStoreTermVectorOffsets(true)
+  termVectoredTextFieldType.setStoreTermVectorPositions(true)
 
   private def documentId(tid: TypedId): String = {
     tid match {
@@ -29,9 +35,9 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
 
   override def insert(id: TypedId, props: Map[String, Any]): Unit = {
     val document = new Document
-    document.add(new StoredField("_id", documentId(id)))
+    document.add(new StringField("_id", documentId(id), Store.YES))
     props.foreach { x =>
-      document.add(new StoredField(x._1, x._2.toString))
+      document.add(new Field(x._1, x._2.toString, termVectoredTextFieldType))
     }
     indexWriter.addDocument(document)
     indexWriter.commit()
@@ -41,9 +47,15 @@ class LucenePropertyStore(dir: File) extends PropertyStore {
     indexWriter.deleteDocuments(new Term("_id", documentId(id)))
 
   override def lookup(id: TypedId): Option[Map[String, Any]] = {
+    val newReader = DirectoryReader.openIfChanged(reader)
+    if (newReader != null) {
+      reader.close()
+      reader = newReader
+      searcher = new IndexSearcher(reader)
+    }
     val doc = new collection.mutable.HashMap[String, Any]()
     val idQuery = new QueryParser("_id", analyzer).parse(documentId(id))
-    val hits = searcher.search(idQuery,1)
+    val hits = searcher.search(idQuery,1000)
     if (hits.totalHits == 0) return None
     val itl = searcher.doc(hits.scoreDocs.head.doc).getFields().iterator()
     while (itl.hasNext()) {
--- lucene-backend/src/test/scala/cn/pandadb/LuceneStoreTest.scala
+++ lucene-backend/src/test/scala/cn/pandadb/LuceneStoreTest.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import cn.pandadb.kernel.impl.{SimpleGraphRAM, SimplePropertyStore}
 import cn.pandadb.kernel.store.{FileBasedIdGen, LabelStore, LogStore, NodeStore, NodeStoreImpl, RelationStore, RelationStoreImpl}
-import cn.pandadb.kernel.GraphFacade
+import cn.pandadb.kernel.{GraphFacade, NodeId, PropertyStore}
 import cn.pandadb.kernel.lucene.LucenePropertyStore
 import org.apache.commons.io.FileUtils
 import org.junit.{Assert, Before, Test}
@@ -17,6 +17,7 @@ class LuceneStoreTest {
   var logs: LogStore = _
   var nodeLabelStore: LabelStore = _
   var relLabelStore: LabelStore = _
+  var propStore: PropertyStore = _
   var memGraph: GraphFacade = _
 
   @Before
@@ -34,59 +35,30 @@ class LuceneStoreTest {
     logs = new LogStore(new File("./testdata/output/logs"))
     nodeLabelStore = new LabelStore(new File("./testdata/output/nodelabels"))
     relLabelStore = new LabelStore(new File("./testdata/output/rellabels"))
+    propStore = new LucenePropertyStore(new File("./testdata/output/properties"))
+//    propStore = new SimplePropertyStore()
 
     memGraph = new GraphFacade(nodes, rels, logs, nodeLabelStore, relLabelStore,
       new FileBasedIdGen(new File("./testdata/output/nodeid"), 100),
       new FileBasedIdGen(new File("./testdata/output/relid"), 100),
       //new DirectGraphRAMImpl(),
       new SimpleGraphRAM(),
-      new LucenePropertyStore(new File("./testdata/output/properties")),
+      propStore,
       { }
     )
   }
 
   @Test
-  def test1(): Unit = {
-    Assert.assertEquals(0, nodes.loadAll().size)
-    Assert.assertEquals(0, rels.loadAll().size)
-    Assert.assertEquals(0, logs._store.loadAll().size)
-
-    memGraph.addNode(Map("name" -> "1")).addNode(Map("name" -> "2")).addRelation("1->2", 1, 2, Map())
-
-    //nodes: {1,2}
-    //rels: {1}
-    Assert.assertEquals(3, logs._store.loadAll().size)
-    Assert.assertEquals(0, nodes.loadAll().size)
-    Assert.assertEquals(0, rels.loadAll().size)
-
+  def testLunceneProps(): Unit = {
+    memGraph.addNode(Map("Name" -> "bluejoe", "age" -> 40), "person")
+    memGraph.addNode(Map("Name" -> "alex", "age" -> 20), "person")
+    memGraph.addNode(Map("Name" -> "simba", "age" -> 10), "person")
+    memGraph.addRelation("knows", 1L, 2L, Map())
+    memGraph.addRelation("knows", 2L, 3L, Map())
     memGraph.mergeLogs2Store()
-
-    Assert.assertEquals(0, logs._store.loadAll().size)
-    Assert.assertEquals(List(1, 2), nodes.loadAll().toSeq.map(_.id).sorted)
-    Assert.assertEquals(List(1), rels.loadAll().toSeq.map(_.id).sorted)
-
-    memGraph.addNode(Map("name" -> "3"))
-    //nodes: {1,2,3}
-    memGraph.mergeLogs2Store()
-
-    Assert.assertEquals(0, logs._store.loadAll().size)
-    Assert.assertEquals(List(1, 2, 3), nodes.loadAll().toSeq.map(_.id).sorted)
-
-    memGraph.deleteNode(2)
-    //nodes: {1,3}
-    memGraph.mergeLogs2Store()
-    Assert.assertEquals(List(1, 3), nodes.loadAll().toSeq.map(_.id).sorted)
-
-    memGraph.addNode(Map("name" -> "4")).deleteNode(1L)
-    //nodes: {3,4}
-    memGraph.mergeLogs2Store()
-    Assert.assertEquals(List(3, 4), nodes.loadAll().toSeq.map(_.id).sorted)
-
-    memGraph.addNode(Map("name" -> "5")).addNode(Map("name" -> "6")).deleteNode(5L).deleteNode(3L)
-    //nodes: {4,6}
-    memGraph.mergeLogs2Store()
-    Assert.assertEquals(List(4, 6), nodes.loadAll().toSeq.map(_.id).sorted)
-
+    var res = propStore.lookup(NodeId(1L))
+    println(res)
+    Assert.assertEquals("1_1", res.get.get("_id").get)
     memGraph.close()
   }
 
@@ -97,73 +69,12 @@ class LuceneStoreTest {
     memGraph.addNode(Map("Name" -> "simba", "age" -> 10), "person")
     memGraph.addRelation("knows", 1L, 2L, Map())
     memGraph.addRelation("knows", 2L, 3L, Map())
-
     var res: CypherResult = null
-    res = memGraph.cypher("match (n) return n")
+    memGraph.mergeLogs2Store()
+    res = memGraph.cypher("match (n) where n.name='bluejoe' return n")
     res.show
-    Assert.assertEquals(3, res.records.size)
-
-    res = memGraph.cypher("match (m)-[r]->(n) return m,r,n")
-    res.show
-    val rec = res.records.collect
-    Assert.assertEquals(2, rec.size)
-
-    Assert.assertEquals(1, rec.apply(0).apply("m").cast[Node[Long]].id)
-    Assert.assertEquals(2, rec.apply(0).apply("n").cast[Node[Long]].id)
-    Assert.assertEquals(1, rec.apply(0).apply("r").cast[Relationship[Long]].id)
-
-    Assert.assertEquals(2, rec.apply(1).apply("m").cast[Node[Long]].id)
-    Assert.assertEquals(3, rec.apply(1).apply("n").cast[Node[Long]].id)
-    Assert.assertEquals(2, rec.apply(1).apply("r").cast[Relationship[Long]].id)
-
+    Assert.assertEquals(1, res.records.size)
     memGraph.close()
   }
 
-  @Test
-  def testLabelStore(): Unit = {
-
-    memGraph.addNode(Map("Name" -> "google"), "Person1")
-    memGraph.addNode(Map("Name" -> "baidu"), "Person2")
-    memGraph.addNode(Map("Name" -> "android"), "Person3")
-    memGraph.addNode(Map("Name" -> "ios"), "Person4")
-    memGraph.addRelation("relation1", 1L, 2L, Map())
-    memGraph.addRelation("relation2", 2L, 3L, Map())
-
-    Assert.assertEquals(4, nodeLabelStore.map.seq.size)
-    Assert.assertEquals(2, relLabelStore.map.seq.size)
-    Assert.assertEquals(6, logs._store.loadAll().size)
-
-    memGraph.mergeLogs2Store()
-    Assert.assertEquals(0, logs._store.loadAll().size)
-
-    memGraph.close()
-
-    val nodeLabelStore2 = new LabelStore(new File("./testdata/output/nodelabels"))
-    val relLabelStore2 = new LabelStore(new File("./testdata/output/rellabels"))
-
-    Assert.assertEquals(4, nodeLabelStore2._store.loadAll().size)
-    Assert.assertEquals(2, relLabelStore2._store.loadAll().size)
-
-  }
-
-  @Test
-  def testRelationStore(): Unit = {
-
-    memGraph.addNode(Map("Name" -> "google"), "Person1")
-    memGraph.addNode(Map("Name" -> "baidu"), "Person2")
-    memGraph.addNode(Map("Name" -> "android"), "Person3")
-    memGraph.addNode(Map("Name" -> "ios"), "Person4")
-    memGraph.addRelation("relation1", 1L, 2L, Map())
-    memGraph.addRelation("relation2", 2L, 3L, Map())
-    memGraph.addRelation("relation3", 3L, 4L, Map())
-
-    memGraph.mergeLogs2Store()
-    Assert.assertEquals(3, rels.loadAll().size)
-
-    memGraph.deleteRelation(1)
-    memGraph.mergeLogs2Store()
-
-    Assert.assertEquals(2, rels.loadAll().size)
-    memGraph.close()
-  }
 }
