be5679799b566314adbb6707cede424f6a501a87
lzxddz <lzxddz@qq.com>
2020-11-26 14:49:07 +0800

    update RocksDBGraphImpl

--- kernel/src/main/scala/cn/pandadb/kernel/kv/RocksDBGraphImpl.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/kv/RocksDBGraphImpl.scala
@@ -1,5 +1,26 @@
 package cn.pandadb.kernel.kv
 
-class RocksDBGraphImpl {
+import cn.pandadb.kernel.GraphRAM
+import cn.pandadb.kernel.store.{StoredNode, StoredRelation}
 
+class RocksDBGraphImpl extends GraphRAM {
+  override def addNode(t: StoredNode): Unit = ???
+
+  override def nodeAt(id: Id): StoredNode = ???
+
+  override def relationAt(id: Id): StoredRelation = ???
+
+  override def deleteNode(id: Id): Unit = ???
+
+  override def addRelation(t: StoredRelation): Unit = ???
+
+  override def deleteRelation(id: Id): Unit = ???
+
+  override def nodes(): Iterator[StoredNode] = ???
+
+  override def rels(): Iterator[StoredRelation] = ???
+
+  override def clear(): Unit = ???
+
+  override def close(): Unit = ???
 }
