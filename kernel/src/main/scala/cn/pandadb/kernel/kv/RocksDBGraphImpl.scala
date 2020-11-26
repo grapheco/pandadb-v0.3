e788032aaf97adbf5708b817fc453ee67cd503c7
Airzihao <airzihao@163.com>
2020-11-26 22:02:28 +0800

    Add interfaces needed in the further impl.

--- kernel/src/main/scala/cn/pandadb/kernel/kv/RocksDBGraphImpl.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/kv/RocksDBGraphImpl.scala
@@ -8,6 +8,7 @@ class RocksDBGraphImpl extends GraphRAM {
 
   override def nodeAt(id: Id): StoredNode = ???
 
+  //TODO: This function not support, no relation id in the kv solution.
   override def relationAt(id: Id): StoredRelation = ???
 
   override def deleteNode(id: Id): Unit = ???
@@ -23,4 +24,25 @@ class RocksDBGraphImpl extends GraphRAM {
   override def clear(): Unit = ???
 
   override def close(): Unit = ???
+
+  // below is the code added by zhaozihao, for possible further use.
+  def relsFrom(id: Id): Iterable[StoredRelation] = ???
+  def relsTo(id: Id): Iterable[StoredRelation] = ???
+
+  def SearchByLabel(label: Label): Iterable[StoredNode] = ???
+  def SearchByType(t: Type): Iterable[StoredRelation] = ???
+
+  def SearchByProperty(stat: Stat): Iterable[StoredNode] = ???
+
+  //essential? could check whether index available in implemantion.
+  def SearchByIndexedProperty(stat: Stat): Iterable[StoredNode] = ???
+  def SearchByCategory(category: Category): Iterable[StoredRelation] = ???
+
+
 }
+
+case class Label(label: String)
+case class Type(t: String)
+
+case class Stat()
+case class Category()
