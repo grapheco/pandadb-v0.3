bb0fd0aa1f5137f87d950a854b72e474ce62605c
胡川 <2585814664@qq.com>
2020-11-27 17:27:50 +0800

    Add base function in NodeIndex

--- kernel/src/test/scala/cn/pandadb/kv/NodeIndexTest.scala
+++ kernel/src/test/scala/cn/pandadb/kv/NodeIndexTest.scala
@@ -1,5 +1,7 @@
 package cn.pandadb.kv
 
+import java.nio.ByteBuffer
+
 import cn.pandadb.kernel.kv.{NodeIndex, RocksDBStorage}
 import org.junit.{After, Assert, Before, Test}
 import org.rocksdb.{ReadOptions, RocksDB}
@@ -12,10 +14,17 @@ class NodeIndexTest extends Assert {
 
   @Test
   def rocksDBTest = {
-    val db:RocksDB = RocksDBStorage.getDB(path)
+    val db:RocksDB = RocksDBStorage.getDB(path+"/test1")
     Assert.assertNotNull(db)
 
+    db.put("aaa".getBytes(),"bbb".getBytes())
     db.put("hello".getBytes(),"world".getBytes())
+    db.put("hello1".getBytes(),"world1".getBytes())
+    db.put("hello2".getBytes(),"world2".getBytes())
+    db.put("eee".getBytes(),"fff".getBytes())
+    db.put("zzz".getBytes(),"zzzz".getBytes())
+    db.put("hello3".getBytes(),"world3".getBytes())
+    db.put("hhh".getBytes(),"hhh".getBytes())
 
     Assert.assertArrayEquals(db.get("hello".getBytes()), "world".getBytes())
     Assert.assertNull(db.get("hel".getBytes()))
@@ -23,8 +32,12 @@ class NodeIndexTest extends Assert {
     val iter = db.newIterator()
     val prefix = "hel".getBytes()
     iter.seek(prefix)
-    while(iter.isValid&&iter.key().startsWith(prefix)) {
-      Assert.assertArrayEquals(iter.value(), "world".getBytes())
+    while(iter.isValid) {
+      println(iter.value().length)
+      if (iter.value().length>0) {
+        println(new String(iter.value()))
+      }
+      //      Assert.assertArrayEquals(iter.value(), "world".getBytes())
       iter.next()
     }
     iter.close()
@@ -46,11 +59,11 @@ class NodeIndexTest extends Assert {
       ni.writeIndexRow(1002.toLong, (node/10).toLong, node.toLong)
     }
 
-    // find 7,14,21
+    // find 0,7,14,21...
     Assert.assertArrayEquals(ni.find(1001.toLong, 7.toLong).toArray,
       Array[Long](0,7,14,21,28,35,42,49)
     )
-    // find 70-79
+    // find 30-39
     Assert.assertArrayEquals(ni.find(1002.toLong, 3.toLong).toArray,
       Array[Long](30,31,32,33,34,35,36,37,38,39)
     )
