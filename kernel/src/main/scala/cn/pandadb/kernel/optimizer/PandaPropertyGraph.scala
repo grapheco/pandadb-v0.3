3f3fb56b5b730da669e94e60a0d315cbe030a266
codeBabyLin <1163718576@qq.com>
2020-12-07 17:01:00 +0800

    update interface

--- kernel/src/main/scala/cn/pandadb/kernel/optimizer/PandaPropertyGraph.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/optimizer/PandaPropertyGraph.scala
@@ -1,6 +1,6 @@
 package cn.pandadb.kernel.optimizer
 
-import cn.pandadb.kernel.kv.NFPredicate
+import cn.pandadb.kernel.kv.{NFLabels, NFPredicate}
 import cn.pandadb.kernel.optimizer.costore.LynxNode
 import org.opencypher.lynx.{LynxPlannerContext, LynxRecords, LynxSession, LynxTable, PropertyGraphScan, RecordHeader}
 import org.opencypher.lynx.graph.{LynxPropertyGraph, ScanGraph}
@@ -17,28 +17,53 @@ import org.parboiled.scala.utils.Predicate
 class PandaPropertyGraph[Id](scan: PandaPropertyGraphScan[Id])(implicit override val session: LynxSession) extends ScanGraph[Id](scan)(session) {
   def getRecorderNumberFromPredicate(predicate: NFPredicate): Int = ???
 
-  def isNFPredicateWithIndex(predicate: NFPredicate): Boolean = ???
+  def isNFPredicateWithIndex(predicate: NFPredicate): Boolean = {
+    //predicate match {
+      //case x:NFLabels => x.labels.map(scan.isLabelWithIndex(_)).reduce(_|_)
+    //}
+  }
 
-  def isNFPredicatesWithIndex(predicate: Array[NFPredicate]): Boolean = ???
 
-  def getNodesByFilter(predicate: Array[NFPredicate], name: String, nodeCypherType: CTNode): LynxRecords = ???
+  def isNFPredicatesWithIndex(predicate: Array[NFPredicate]): Boolean = {
+    //predicate.map()
+  }
+
+  def getNodesByFilter(predicate: Array[NFPredicate], name: String, nodeCypherType: CTNode): LynxRecords = {
+/*    var nodes:Seq[Node[Id]] = Seq[Node[Id]]()
+    var isFirst: Boolean = true
+    predicate.foreach(u =>{
+      if(!isFirst){
+        nodes = nodes.intersect(scan.allNodes(u).toSeq)
+      }
+      else {
+        nodes = scan.allNodes(u).toSeq
+        isFirst = false
+      }
+    })*/
+
+   // val nodes = predicate.map(scan.allNodes(_).toSeq).reduce(_.intersect(_))
+   // new LynxRecords(
+   //   RecordHeader(Map(NodeVar(name)(CTNode) -> name)),
+   //   LynxTable(Seq(name -> CTNode), nodes.map(Seq(_)))
+   // )
+  }
 }
 
 
 
 trait PandaPropertyGraphScan[Id] extends PropertyGraphScan[Id] {
-  def isPropertyWithIndex(propertyName: String): Boolean = ???
+  def isPropertyWithIndex(labels: Set[String], propertyName: String): Boolean = ???
 
-  def isLabelWithIndex(label: String): Boolean = ???
+  // def isLabelWithIndex(label: String): Boolean = ???
 
-  def isPopertysWithIndex(propertyName1: String, propertyName2: String): Boolean = ???
+ // def isPopertysWithIndex(propertyName1: String, propertyName2: String): Boolean = ???
 
-  def isLabelAndPropertyWithIndex(propertyName: String, label: String): Boolean = ???
+ // def isLabelAndPropertyWithIndex(propertyName: String, label: String): Boolean = ???
 
-  def getRecorderNumbersFromProperty(propertyName: String): Int = ???
+  //def getRecorderNumbersFromProperty(labels: Set[String], propertyName: String): Int = ???
 
-  def getRecorderNumbersFromLabel(label: String): Int = ???
+ // def getRecorderNumbersFromLabel(label: String): Int = ???
 
-  def allNodes(predicate: NFPredicate): Iterable[Node[Id]] = ???
+  def allNodes(predicate: NFPredicate, labels: Set[String]): Iterable[Node[Id]] = ???
 
 }
