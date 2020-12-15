b903508f834a42dcdb46adf28c1a480e506d818a
excelwang <excelwang@users.noreply.github.com>
2020-12-15 16:35:42 +0800

    add query batch gen methods

--- kernel/src/test/scala/cn/pandadb/kv/performance/querys.scala
+++ kernel/src/test/scala/cn/pandadb/kv/performance/querys.scala
@@ -1,5 +1,8 @@
 package cn.pandadb.kv.performance
 
+import scala.collection.mutable
+import scala.util.Random
+
 object querys {
   def getCypherCreateNode(id: Long, idStr: String, flag: Boolean, labels: Array[String] = Array()): String = {
     var labelStr = new StringBuilder()
@@ -21,7 +24,7 @@ object querys {
     s"match (n) where n.${equalString} return n${limitStr}"
   }
 
-  def getCypherQueryByAttrGt(gtString: String, limit: Int = 0): String = {
+  def getCypherQueryByAttrGtLt(gtString: String, limit: Int = 0): String = {
     val limitStr = limit match {
       case 0 => ""
       case _ => s" limit ${limit}"
@@ -34,4 +37,32 @@ object querys {
     labels.foreach(labelStr += s":${_}")
     s"match (f)-[r${labelStr}]->(t) where f.id_p=${fromId_p} return Count(t)"
   }
+
+  def genBatchCreation(startId: Long, endId: Long): Array[String] = {
+    val resArrayNode: mutable.ArrayBuffer[String] = new mutable.ArrayBuffer[String]()
+    val resArrayRel: mutable.ArrayBuffer[String] = new mutable.ArrayBuffer[String]()
+    for(id <- startId until endId){
+      val labels =  Array(s"label${id%10}")
+      resArrayNode += getCypherCreateNode(id, s"${id}", id%2==0, labels)
+      if (id!=startId){
+        resArrayRel += getCypherCreateRel(id, Random.nextInt(id.toInt), labels)
+      }
+    }
+    (resArrayNode++resArrayRel).toArray
+  }
+
+  def genBatchQuery(randomUntil: Int): Array[String] = {
+    val resArray: mutable.ArrayBuffer[String] = new mutable.ArrayBuffer[String]()
+    val id = Random.nextInt(randomUntil)
+    resArray+=getCypherQueryByAttrEqual(s"id_p=${id}", 1)
+    resArray+=getCypherQueryByAttrEqual(s"id_p=${id}")
+    resArray+=getCypherQueryByAttrGtLt(s"id_p<${id}", 1)
+    resArray+=getCypherQueryByAttrGtLt(s"id_p<${id}", 10)
+    resArray+=getCypherQueryByAttrGtLt(s"id_p<${id}")
+    resArray+=getCypherQueryByRelFrom(id)
+    resArray+=getCypherQueryByRelFrom(id, Array(s"label${id%10}"))
+    resArray.toArray
+  }
+  val demoQuerys = genBatchCreation(1, 5) ++ genBatchQuery(5)
+  println(demoQuerys)
 }
