a3a1283d095cd97bd5ad4f553b02043d1056d24e
excelwang <excelwang@users.noreply.github.com>
2020-12-21 17:54:45 +0800

    add RelationSimulator to mimic join or expands strategies when filter relations

---
+++ kernel/src/main/scala/cn/pandadb/kernel/optimizer/Simulator.scala
@@ -0,0 +1,57 @@
+package cn.pandadb.kernel.optimizer
+
+import cn.pandadb.kernel.kv.RocksDBGraphAPI
+
+class RelationSimulator(api: RocksDBGraphAPI){
+  //cypher: String = "match (n:label1)->[r:type1]->(m: label2) return count(m)"
+  val leftLabelId=1
+  val rightLabelId=2
+  val relationTypeId=1
+  def OnTheFlyNodeFirst(leftFirst: Boolean = true): Long ={
+    var result: Long = 0
+    if(leftFirst){
+      api.findNodes(leftLabelId).map(nodeId => {
+        api.findOutEdgeRelations(nodeId, relationTypeId).map(r=>{
+          if (api.nodeAt(r.to).labelIds.contains(rightLabelId)){
+            result += 1
+          }
+        })
+      })
+    }
+    else{
+      api.findNodes(rightLabelId).map(nodeId => {
+        api.findInEdgeRelations(nodeId, relationTypeId).map(r=>{
+          if (api.nodeAt(r.from).labelIds.contains(leftLabelId)){
+            result += 1
+          }
+        })
+      })
+    }
+    result
+  }
+  def OnTheFlyRelationFirst(): Long ={
+    var result: Long = 0
+    api.findRelations(relationTypeId).map(rId=>{
+      val r = api.relationAt(rId)
+      if(api.nodeAt(r.from).labelIds.contains(leftLabelId) &&
+        api.nodeAt(r.to).labelIds.contains(rightLabelId)){
+        result += 1
+      }
+    })
+    result
+  }
+
+  def PreloadThenJoin(): Long ={
+    var result: Long = 0
+    val leftNodeIds = api.findNodes(leftLabelId).toArray[Long]
+    val rightNodeIds = api.findNodes(rightLabelId).toArray[Long]
+    api.findRelations(relationTypeId).map(rId=>{
+      val r = api.relationAt(rId)
+      if(leftNodeIds.contains(r.from) && rightNodeIds.contains(r.to)){
+        result += 1
+      }
+    })
+    result
+  }
+
+}
