61c6f7d4b525645c881d10664ebf330ced705f3b
excelwang <excelwang@users.noreply.github.com>
2020-12-31 16:23:55 +0800

    fix query planner simulation

--- kernel/src/main/scala/cn/pandadb/kernel/optimizer/simulator/ExpandOrJoinSimulator.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/optimizer/simulator/ExpandOrJoinSimulator.scala
@@ -1,54 +1,63 @@
-//package cn.pandadb.kernel.optimizer.simulator
-//
-//import cn.pandadb.kernel.kv.RocksDBGraphAPI
-//
-//class ExpandOrJoinSimulator(api: RocksDBGraphAPI, leftLabelId:Int=1, rightLabelId:Int=2, relationTypeId:Int=1){
-//  //cypher: String = "match (n:label1)->[r:type1]->(m: label2) return count(m)"
-//  def OnTheFlyExpandNodeFirst(leftFirst: Boolean = true): Long ={
-//    var result: Long = 0
-//    if(leftFirst){
-//      api.findNodes(leftLabelId).map(nodeId => {
-//        api.findOutRelations(nodeId, relationTypeId).map(r=>{
-//          if (api.nodeAt(r.to).labelIds.contains(rightLabelId)){
-//            result += 1
-//          }
-//        })
-//      })
-//    }
-//    else{
-//      api.findNodes(rightLabelId).map(nodeId => {
-//        api.findInRelations(nodeId, relationTypeId).map(r=>{
-//          if (api.nodeAt(r.from).labelIds.contains(leftLabelId)){
-//            result += 1
-//          }
-//        })
-//      })
-//    }
-//    result
-//  }
-//  def OnTheFlyExpandRelationFirst(): Long ={
-//    var result: Long = 0
-//    api.getRelationsByType(relationTypeId).map(rId=>{
-//      val r = api.relationAt(rId)
-//      if(api.nodeAt(r.from).labelIds.contains(leftLabelId) &&
-//        api.nodeAt(r.to).labelIds.contains(rightLabelId)){
-//        result += 1
-//      }
-//    })
-//    result
-//  }
-//
-//  def PreloadThenJoin(): Long ={
-//    var result: Long = 0
-//    val leftNodeIds = api.findNodes(leftLabelId).toArray[Long]
-//    val rightNodeIds = api.findNodes(rightLabelId).toArray[Long]
-//    api.getRelationsByType(relationTypeId).map(rId=>{
-//      val r = api.relationAt(rId)
-//      if(leftNodeIds.contains(r.from) && rightNodeIds.contains(r.to)){
-//        result += 1
-//      }
-//    })
-//    result
-//  }
-//
-//}
+package cn.pandadb.kernel.optimizer.simulator
+
+import cn.pandadb.kernel.kv.node.NodeStore
+import cn.pandadb.kernel.kv.relation.RelationStoreAPI
+
+class ExpandOrJoinSimulator(nodeApi: NodeStore, relApi: RelationStoreAPI, leftLabelId:Int=1, rightLabelId:Int=2, relationTypeId:Int=1){
+  //cypher: String = "match (n:label1)->[r:type1]->(m: label2) return count(m)"
+  def OnTheFlyExpandNodeFirst(leftFirst: Boolean = true): Long ={
+    var result: Long = 0
+    if(leftFirst){
+      nodeApi.getNodeIdsByLabel(leftLabelId).map(nodeId => {
+        relApi.findOutRelations(nodeId, relationTypeId).map(r=>{
+          if (!nodeApi.get(r.to, rightLabelId).isEmpty){
+            result += 1
+          }
+        })
+      })
+    }
+    else{
+      nodeApi.getNodeIdsByLabel(rightLabelId).map(nodeId => {
+        relApi.findInRelations(nodeId, relationTypeId).map(r=>{
+          if (!nodeApi.get(r.from, leftLabelId).isEmpty){
+            result += 1
+          }
+        })
+      })
+    }
+    result
+  }
+  def OnTheFlyExpandRelationFirst(): Long ={
+    var result: Long = 0
+    relApi.getRelationIdsByRelationType(relationTypeId).map(rId=>{
+      val r = relApi.getRelationById(rId).get
+      if(!nodeApi.get(r.from, leftLabelId).isEmpty && !nodeApi.get(r.to, rightLabelId).isEmpty){
+        result+=1
+      }
+    })
+    result
+  }
+
+  def PreloadThenJoin(): Long ={
+    var result: Long = 0
+    val leftNodeIds = nodeApi.getNodeIdsByLabel(leftLabelId)
+    val rightNodeIds = nodeApi.getNodeIdsByLabel(rightLabelId)
+    val rels = relApi.getRelationIdsByRelationType(relationTypeId).map(rid => {
+      relApi.getRelationById(rid).get
+    })
+    //here we should choose the larger nodeIds to map at first, but we cannot get the iterator length. so choose left
+    // for test
+    val wideTable = leftNodeIds.flatMap(lid => {
+      rels.filter(_.from==lid).map(r=>{
+        r.to -> r.from
+      })
+    })
+    rightNodeIds.map(rid=>{
+      if(wideTable.filter(_._1==rid).isEmpty){
+        result+=1
+      }
+    })
+    result
+  }
+
+}
