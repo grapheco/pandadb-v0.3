e36114180b9f8797657f9de6c812cf11e8a11fb8
Airzihao <airzihao@163.com>
2021-01-16 11:25:29 +0800

    ImportCmd finished.

--- tools/src/main/scala/cn/pandadb/tools/importer/ImportCmd.scala
+++ tools/src/main/scala/cn/pandadb/tools/importer/ImportCmd.scala
@@ -10,6 +10,10 @@ import java.io.File
  */
 case class ImportCmd(args: Array[String]) {
   val funcName: String = args(0)
+  val database: File = {
+    val path = _getArgByName("db-path")
+    new File(path)
+  }
 
   val nodeFileList: List[File] = {
     val nodesFilesPath: Array[String] = _getArgByName("nodes").split(",")
@@ -21,6 +25,7 @@ case class ImportCmd(args: Array[String]) {
   }
   val exportDBPath: File = {
     val dbFile = new File(_getArgByName("db-path"))
+    if (!dbFile.exists()) dbFile.mkdirs()
     if (!dbFile.isDirectory || dbFile.listFiles().length != 0) {
       throw new Exception(s"The export db path $dbFile is not an empty directory.")
     }
@@ -30,21 +35,28 @@ case class ImportCmd(args: Array[String]) {
   val delimeter: String = {
     val delimeter = _getArgByName("delimeter")
     if(delimeter.length>1) throw new Exception(s"The delimeter takes only one character, modify your input $delimeter please.")
-    if(delimeter.length == 1) delimeter
+    if(delimeter.length == 1) _transferDelimeter(delimeter)
     else ","
   }
 
   val arrayDelimeter: String = {
     val arrayDelimeter: String = _getArgByName("array-delimeter")
     if (arrayDelimeter.length>1) throw new Exception(s"The array-delimeter takes only one character, modify your input $arrayDelimeter please.")
-    if(arrayDelimeter.length == 1) arrayDelimeter
+    if(arrayDelimeter.length == 1) _transferDelimeter(delimeter)
     else "|"
   }
 
   private def _getArgByName(name: String): String = {
-    val index: Int = args.indexWhere(arg => arg.startsWith(s"--$name="))
-    if (index == -1) ""
-    else args(index).replace("--$name=", "")
+    val filtered: Array[String] = args.filter(arg => arg.startsWith(s"--$name=")).map(arg => arg.replace(s"--$name=", ""))
+//    val index: Int = args.indexWhere(arg => arg.startsWith(s"--$name="))
+    if (filtered.length == 0) ""
+    else filtered.mkString(",")
+  }
+
+  private def _transferDelimeter(srcDelimeter: String): String = {
+    val needTransfer: Boolean = srcDelimeter.equals("|")
+    if (needTransfer) s"\\$srcDelimeter"
+    else srcDelimeter
   }
 
 }
