ac829909983564e727579c76eb1aa71f6304a278
LianxinGao <503773453@qq.com>
2021-03-17 14:08:26 +0800

    update id func

--- kernel/src/main/scala/cn/pandadb/kernel/kv/lynx/procedure/functions/IdFunction.scala
+++ kernel/src/main/scala/cn/pandadb/kernel/kv/lynx/procedure/functions/IdFunction.scala
@@ -1,7 +1,8 @@
 package cn.pandadb.kernel.kv.lynx.procedure.functions
 
 import cn.pandadb.kernel.kv.lynx.procedure.PandaFunction
-import org.grapheco.lynx.{CallableProcedure, LynxBoolean, LynxInteger, LynxNode, LynxRelationship, LynxType, LynxValue}
+import cn.pandadb.kernel.store.{PandaNode, PandaRelationship}
+import org.grapheco.lynx.{CallableProcedure, LynxBoolean, LynxInteger, LynxNode, LynxNull, LynxRelationship, LynxType, LynxValue}
 import org.opencypher.v9_0.util.symbols.CTInteger
 
 case object IdFunction extends PandaFunction{
@@ -13,9 +14,9 @@ case object IdFunction extends PandaFunction{
         override val outputs: Seq[(String, LynxType)] = Seq("id"->CTInteger)
 
         override def call(args: Seq[LynxValue]): Iterable[Seq[LynxValue]] = {
-          Iterable(Seq(LynxInteger(args match {
-            case sNode: Seq[LynxNode] => sNode.head.id.value.asInstanceOf[Long]
-            case sRel: Seq[LynxRelationship] => sRel.head.id.value.asInstanceOf[Long]
+          Iterable(Seq(LynxInteger(args.head match {
+            case sNode: LynxNode => sNode.id.value.asInstanceOf[Long]
+            case sRel: LynxRelationship => sRel.id.value.asInstanceOf[Long]
           })))
         }
       }
