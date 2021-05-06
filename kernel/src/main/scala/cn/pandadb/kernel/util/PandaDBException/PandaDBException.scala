8ab9e5cd0892a9c3206c2c0bd7e6152a7cadf174
Airzihao <airzihao@163.com>
2021-05-06 10:03:33 +0800

    PandaDBException.

---
+++ kernel/src/main/scala/cn/pandadb/kernel/util/PandaDBException/PandaDBException.scala
@@ -0,0 +1,11 @@
+package cn.pandadb.kernel.util.PandaDBException
+
+/**
+ * @Author: Airzihao
+ * @Description:
+ * @Date: Created at 9:55 2021/5/6
+ * @Modified By:
+ */
+class PandaDBException(msg: String) extends Exception{
+  override def getMessage: String = msg
+}
