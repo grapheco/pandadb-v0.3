d3e76f23b4a20900114f7d1be6c875964a8ac95e
LianxinGao <503773453@qq.com>
2022-01-17 09:57:22 +0800

    add demo

---
+++ packaging/src/main/distribution/bin/demo-importer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+NODESCOUNT=0
+RELSCOUNT=0
+DELIMETER="--delimeter=,"
+ARRAY_DELIMETER="--array-delimeter=|"
+
+get_params_func(){
+for arg in $@
+do
+  if [[ $arg == "--kv-hosts"* ]]
+  then
+    KV_HOSTS=$arg
+  fi
+done
+}
+
+get_file_nodes_names(){
+for file_name in ${PANDADB_DEMO_NODES}/*
+do
+  tmpFile="--nodes="${PANDADB_HOME}/demo/nodes/`basename $file_name`
+  NODES[$NODESCOUNT]=$tmpFile
+  let NODESCOUNT++
+done
+}
+get_file_rels_names(){
+for file_name in ${PANDADB_DEMO_RELS}/*
+do
+  tmpFile="--rels="${PANDADB_HOME}/demo/rels/`basename $file_name`
+  RELS[$RELSCOUNT]=$tmpFile
+  let RELSCOUNT++
+done
+}
+
+main(){
+PANDADB_HOME="$(cd "$(dirname "$0")"/.. && pwd)"
+PANDADB_LAB=$PANDADB_HOME"/lib"
+PANDADB_DEMO_NODES=$PANDADB_HOME"/demo/nodes"
+PANDADB_DEMO_RELS=$PANDADB_HOME"/demo/relations"
+
+get_params_func "$@"
+get_file_nodes_names
+get_file_rels_names
+
+java -cp "$PANDADB_LAB""//*" "cn.pandadb.tools.importer.PandaImporter" "${NODES[@]}" "${RELS[@]}" "--delimeter=|" "--array-delimeter=," "$KV_HOSTS"
+
+}
+
+main "$@"
+
